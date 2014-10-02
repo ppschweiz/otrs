83fdc2ebe7bd3687243e55737aa1d3e6caafdb38
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-10-02 22:01:05 +0200

    Initial commit

---
+++ Config.pm
@@ -0,0 +1,154 @@
+# --
+# Kernel/Config.pm - Config file for OTRS kernel
+# Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+#  Note:
+#
+#  -->> Most OTRS configuration should be done via the OTRS web interface
+#       and the SysConfig. Only for some configuration, such as database
+#       credentials and customer data source changes, you should edit this
+#       file. For changes do customer data sources you can copy the definitions
+#       from Kernel/Config/Defaults.pm and paste them in this file.
+#       Config.pm will not be overwritten when updating OTRS.
+# --
+
+package Kernel::Config;
+
+#use strict;
+use warnings;
+use utf8;
+
+sub Load {
+    my $Self = shift;
+
+    # ---------------------------------------------------- #
+    # database settings                                    #
+    # ---------------------------------------------------- #
+
+    # The database host
+    $Self->{DatabaseHost} = $ENV{'MYSQL_PORT_3306_TCP_ADDR'};
+
+    # The database name
+    $Self->{Database} = $ENV{'MYSQL_DATABASE'};
+
+    # The database user
+    $Self->{DatabaseUser} = $ENV{'MYSQL_USERNAME'};
+
+    # The password of database user. You also can use bin/otrs.CryptPassword.pl
+    # for crypted passwords
+    $Self->{DatabasePw} = $ENV{'MYSQL_PASSWORD'};
+
+    # The database DSN for MySQL ==> more: "perldoc DBD::mysql"
+    $Self->{DatabaseDSN} = "DBI:mysql:database=$Self->{Database};host=$Self->{DatabaseHost};";
+
+    # ---------------------------------------------------- #
+    # fs root directory
+    # ---------------------------------------------------- #
+    $Self->{Home} = '/opt/otrs';
+
+    # ---------------------------------------------------- #
+    # insert your own config settings "here"               #
+    # config settings taken from Kernel/Config/Defaults.pm #
+    # ---------------------------------------------------- #
+    # $Self->{SessionUseCookie} = 0;
+    # $Self->{CheckMXRecord} = 0;
+    $Self->{'DefaultCharset'} = 'utf-8';
+
+    delete $Self->{'PreferencesGroups'}->{'SpellDict'};
+    delete $Self->{'SendmailBcc'};
+    $Self->{'Organization'} =  'Piratenpartei Schweiz';
+    $Self->{'AdminEmail'} =  'admin@piratenpartei.ch';
+    $Self->{'HttpType'} =  'https';
+    $Self->{'FQDN'} =  'info.piratenpartei.ch';
+    $Self->{'SecureMode'} =  '1';
+    $Self->{'MinimumLogLevel'} =  'info';
+    
+    $Self->{'AuthModule'} = 'Kernel::System::Auth::LDAP';
+    $Self->{'AuthModule::UseSyncBackend'} = 'AuthSyncBackend';
+    $Self->{'AuthModule::LDAP::Host'} = $ENV{'LDAP_PORT_389_TCP_ADDR'};
+    $Self->{'AuthModule::LDAP::BaseDN'} = $ENV{'LDAP_BASEDN'};
+    $Self->{'AuthModule::LDAP::UID'} = $ENV{'LDAP_UID'};
+
+    $Self->{'AuthModule::LDAP::SearchUserDN'} = $ENV{'LDAP_USERNAME'};
+    $Self->{'AuthModule::LDAP::SearchUserPw'} = $ENV{'LDAP_PASSWORD'};
+
+    $Self->{'AuthSyncModule'} = 'Kernel::System::Auth::Sync::LDAP';
+    $Self->{'AuthSyncModule::LDAP::Host'} = $ENV{'LDAP_PORT_389_TCP_ADDR'};
+    $Self->{'AuthSyncModule::LDAP::BaseDN'} = $ENV{'LDAP_BASEDN'};
+    $Self->{'AuthSyncModule::LDAP::UID'} = $ENV{'LDAP_UID'};
+    $Self->{'AuthSyncModule::LDAP::SearchUserDN'} = $ENV{'LDAP_USERNAME'};
+    $Self->{'AuthSyncModule::LDAP::SearchUserPw'} = $ENV{'LDAP_PASSWORD'};
+    $Self->{'AuthSyncModule::LDAP::UserSyncMap'} = {
+       # DB -> LDAP
+       UserFirstname => 'givenName',
+       UserLastname  => 'sn',
+       UserEmail     => 'mail',
+    };
+    $Self->{AuthSyncModule::LDAP::AccessAttr} = 'member';
+    $Self->{AuthSyncModule::LDAP::UserAttr} = 'DN';
+
+    # AuthSyncModule::LDAP::UserSyncRolesDefinition
+    # (If "LDAP" was selected for AuthModule and you want to sync LDAP groups to otrs roles, define the following.)
+    $Self->{AuthSyncModule::LDAP::UserSyncRolesDefinition} = {
+       'cn=DI,dc=workgroups,dc=piratenpartei,dc=ch' => { 'AG DI' => 1, },
+       'cn=KAMP,dc=workgroups,dc=piratenpartei,dc=ch' => { 'AG KAMP' => 1, },
+       'cn=ROA,dc=workgroups,dc=piratenpartei,dc=ch' => { 'AG ROA' => 1, },
+       'cn=TNT,dc=workgroups,dc=piratenpartei,dc=ch' => { 'AG TNT'=> 1, },
+       'cn=PR,dc=workgroups,dc=piratenpartei,dc=ch' => { 'AG PR'=> 1, },
+       'cn=Board,dc=piratenpartei,dc=ch' => { 'Board' => 1, },
+       'cn=Direction,cn=Board,dc=piratenpartei,dc=ch' => { 'Board' => 1, 'Direction' => 1,},
+       'cn=Presidium,cn=Board,dc=piratenpartei,dc=ch' => { 'Board' => 1, 'Presidium' => 1, },
+       'cn=Board,st=ag,dc=piratenpartei,dc=ch' => { 'Board AG' => 1, },
+       'cn=Board,st=bb,dc=piratenpartei,dc=ch' => { 'Board BB' => 1, },
+       'cn=Board,st=be,dc=piratenpartei,dc=ch' => { 'Board BE' => 1, },
+       'cn=Board,st=fr,dc=piratenpartei,dc=ch' => { 'Board FR' => 1, },
+       'cn=Board,st=ge,dc=piratenpartei,dc=ch' => { 'Board GE' => 1, },
+       'cn=Board,st=ne,dc=piratenpartei,dc=ch' => { 'Board NE' => 1, },
+       'cn=Board,st=os,dc=piratenpartei,dc=ch' => { 'Board OS' => 1, },
+       'cn=Board,st=ti,dc=piratenpartei,dc=ch' => { 'Board TI' => 1, },
+       'cn=Board,st=ts,dc=piratenpartei,dc=ch' => { 'Board TS' => 1, },
+       'cn=Board,st=vd,dc=piratenpartei,dc=ch' => { 'Board VD' => 1, },
+       'cn=Board,st=vs,dc=piratenpartei,dc=ch' => { 'Board VS' => 1, },
+       'cn=Board,st=zh,dc=piratenpartei,dc=ch' => { 'Board ZH' => 1, },
+       'cn=Board,st=zs,dc=piratenpartei,dc=ch' => { 'Board ZS' => 1, },
+       'cn=Board,l=bern,st=be,dc=piratenpartei,dc=ch' => { 'Board Bern' => 1, },
+       'cn=Board,l=winterthur,st=zh,dc=piratenpartei,dc=ch' => { 'Board Winterthur' => 1, },
+       'cn=Board,l=zurich,st=zh,dc=piratenpartei,dc=ch' => { 'Board Zürich' => 1, },
+       'cn=AnK,dc=piratenpartei,dc=ch' => { 'Antragskommission' => 1, },
+       'cn=PG,dc=piratenpartei,dc=ch' => { 'Pirate Court'=> 1, },
+   };
+
+    # ---------------------------------------------------- #
+
+    # ---------------------------------------------------- #
+    # data inserted by installer                           #
+    # ---------------------------------------------------- #
+    # $DIBI$
+
+    # ---------------------------------------------------- #
+    # ---------------------------------------------------- #
+    #                                                      #
+    # end of your own config options!!!                    #
+    #                                                      #
+    # ---------------------------------------------------- #
+    # ---------------------------------------------------- #
+}
+
+# ---------------------------------------------------- #
+# needed system stuff (don't edit this)                #
+# ---------------------------------------------------- #
+use strict;
+use warnings;
+
+use vars qw(@ISA);
+
+use Kernel::Config::Defaults;
+push (@ISA, 'Kernel::Config::Defaults');
+
+# -----------------------------------------------------#
+
+1;
---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM ppschweiz/apache
+
+RUN apt-get update && apt-get -y install libapache2-mod-perl2 libdbd-mysql-perl libtimedate-perl libnet-dns-perl \
+    libnet-ldap-perl libio-socket-ssl-perl libpdf-api2-perl libdbd-mysql-perl libsoap-lite-perl \
+    libgd-text-perl libtext-csv-xs-perl libjson-xs-perl libgd-graph-perl libapache-dbi-perl libmail-imapclient-perl libyaml-libyaml-perl
+
+
+ENV MYSQL_PORT_3306_TCP_ADDR localhost
+ENV MYSQL_PORT_3306_TCP_PORT 3306
+ENV MYSQL_USERNAME otrs
+ENV MYSQL_PASSWORD changeme
+ENV MYSQL_DATABASE otrs
+ENV SMTP_PORT_22_TCP_ADDR mail-1-p.piratenpartei.ch
+ENV SMTP_PORT_22_TCP_PORT 25
+
+ADD otrs-3.2.9.tar.gz /opt/
+RUN ln -s /opt/otrs-3.2.9 /opt/otrs
+RUN useradd -r -d /opt/otrs/ -c 'OTRS user' otrs && usermod -G nogroup otrs
+
+COPY Config.pm /opt/otrs/Kernel/Config.pm
+RUN cp /opt/otrs/Kernel/Config/GenericAgent.pm.dist /opt/otrs/Kernel/Config/GenericAgent.pm
+RUN /opt/otrs/bin/otrs.SetPermissions.pl /opt/otrs --otrs-user=otrs --otrs-group=nogroup --web-user=www-data --web-group=www-data
+RUN /opt/otrs/bin/otrs.CheckModules.pl
+
+RUN a2dissite 000-default
+RUN ln -s /opt/otrs/scripts/apache2-httpd.include.conf /etc/apache2/conf.d/otrs.conf
+RUN echo "RedirectMatch ^/$ /otrs-web/" >> /etc/apache2/conf.d/otrs.conf
+RUN echo "PerlPassEnv MYSQL_PORT_3306_TCP_ADDR" >> /etc/apache2/conf.d/otrs.conf
+RUN echo "PerlPassEnv MYSQL_PORT_3306_TCP_PORT" >> /etc/apache2/conf.d/otrs.conf
+RUN echo "PerlPassEnv MYSQL_USERNAME" >> /etc/apache2/conf.d/otrs.conf
+RUN echo "PerlPassEnv MYSQL_PASSWORD" >> /etc/apache2/conf.d/otrs.conf
+RUN echo "PerlPassEnv MYSQL_DATABASE" >> /etc/apache2/conf.d/otrs.conf
+RUN echo "PerlPassEnv SMTP_PORT_22_TCP_ADDR" >> /etc/apache2/conf.d/otrs.conf
+RUN echo "PerlPassEnv SMTP_PORT_22_TCP_PORT" >> /etc/apache2/conf.d/otrs.conf
+
