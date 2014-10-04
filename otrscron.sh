bb8e054e28fe3f478d6b387d7ac1c5d4ecb0d06b
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-10-04 16:53:41 +0200

    hack for /opt/otrs/Kernel/Config/Files/ZZZAAuto.pm

--- otrscron.sh
+++ otrscron.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 su -s /usr/bin/perl otrs /opt/otrs/bin/otrs.RebuildConfig.pl
+chmod 666 /opt/otrs/Kernel/Config/Files/ZZZAAuto.pm
 while true; do 
 su -s /usr/bin/perl otrs /opt/otrs/bin/otrs.PostMasterMailbox.pl
 sleep 60
