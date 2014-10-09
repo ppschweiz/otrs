6076a6c267cc4331a5a00403853e889010afd13d
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-10-09 18:18:52 +0200

    use builtin polling

--- otrscron.sh
+++ otrscron.sh
@@ -2,4 +2,4 @@
 set -e
 su -s /usr/bin/perl otrs /opt/otrs/bin/otrs.RebuildConfig.pl
 chmod 666 /opt/otrs/Kernel/Config/Files/ZZZAAuto.pm
-su -s /usr/bin/perl otrs /opt/otrs/bin/otrs.PostMasterMailbox.pl -b 60
+su otrs -c "/usr/bin/perl /opt/otrs/bin/otrs.PostMasterMailbox.pl -b 60"
