82dd16316b14a635a898b653f5a34bc39e04c530
J0WI <J0WI@users.noreply.github.com>
2020-10-23 22:57:22 +0200

    Readd Perl shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-su otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Database::Check"
-su otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild"
-su otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Cache::Delete"
+su -s /usr/bin/perl otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Database::Check"
+su -s /usr/bin/perl otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild"
+su -s /usr/bin/perl otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Cache::Delete"
 
 /opt/otrs/bin/otrs.SetPermissions.pl /opt/otrs --otrs-user=www-data --web-group=www-data
 chmod 666 /opt/otrs/Kernel/Config/Files/ZZZAAuto.pm
 
-su www-data -c "/opt/otrs/bin/otrs.Daemon.pl start"
+su -s /usr/bin/perl www-data -c "/opt/otrs/bin/otrs.Daemon.pl start"
 
 exec "$@"
