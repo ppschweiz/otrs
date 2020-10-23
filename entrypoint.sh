04c6592c74402813ec084d24deb87dd6dc8e3088
J0WI <J0WI@users.noreply.github.com>
2020-10-23 23:17:05 +0200

    Revert command changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-su -s /usr/bin/perl otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Database::Check"
-su -s /usr/bin/perl otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild"
-su -s /usr/bin/perl otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Cache::Delete"
+su -s /usr/bin/perl otrs /opt/otrs/bin/otrs.Console.pl Maint::Database::Check
+su -s /usr/bin/perl otrs /opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild
+su -s /usr/bin/perl otrs /opt/otrs/bin/otrs.Console.pl Maint::Cache::Delete
 
 /opt/otrs/bin/otrs.SetPermissions.pl /opt/otrs --otrs-user=www-data --web-group=www-data
 chmod 666 /opt/otrs/Kernel/Config/Files/ZZZAAuto.pm
 
-su -s /usr/bin/perl www-data -c "/opt/otrs/bin/otrs.Daemon.pl start"
+su -s /usr/bin/perl www-data /opt/otrs/bin/otrs.Daemon.pl start
 
 exec "$@"
