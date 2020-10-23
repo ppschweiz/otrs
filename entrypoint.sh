40f19570e6372645b842199b54e50401beff85e2
J0WI <J0WI@users.noreply.github.com>
2020-10-23 22:11:01 +0200

    Check DB connection before rebuild

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,13 @@
 
 set -eu
 
-su -s /usr/bin/perl otrs /opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild
-su -s /usr/bin/perl otrs /opt/otrs/bin/otrs.Console.pl Maint::Cache::Delete
+su otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Database::Check"
+su otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild"
+su otrs -c "/opt/otrs/bin/otrs.Console.pl Maint::Cache::Delete"
+
 /opt/otrs/bin/otrs.SetPermissions.pl /opt/otrs --otrs-user=www-data --web-group=www-data
 chmod 666 /opt/otrs/Kernel/Config/Files/ZZZAAuto.pm
-su -s /usr/bin/perl www-data /opt/otrs/bin/otrs.Daemon.pl start
+
+su www-data -c "/opt/otrs/bin/otrs.Daemon.pl start"
 
 exec "$@"
